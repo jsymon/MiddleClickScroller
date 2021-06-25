d57eb45a2404eaa0c1483fd9cbbb541b168183ce
James Symon <James.Symon@uk.renaultsportracing.com>
2021-06-25 13:36:29 +0100

    Add project files.

---
+++ MiddleClickScrollerFactory.cs
@@ -0,0 +1,29 @@
+﻿
+namespace MiddleClickScroller
+{
+    using System.ComponentModel.Composition;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Utilities;
+
+    [Export(typeof(IMouseProcessorProvider))]
+    [Name("MiddleClickScroller")]
+    [Order(Before = "UrlClickMouseProcessor")]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    internal sealed class MiddleClickScrollFactory : IMouseProcessorProvider
+    {
+        internal const string ADORNER_LAYER_NAME = "MiddleClickScrollLayer";
+        //#pragma warning disable 649
+        [Export]
+        [Name(ADORNER_LAYER_NAME)]
+        [Order(Before = PredefinedAdornmentLayers.Selection)]
+        internal AdornmentLayerDefinition viewLayerDefinition;
+        //#pragma warning restore 649
+
+        public IMouseProcessor GetAssociatedProcessor(IWpfTextView textView)
+        {
+            return MiddleClickScroll.Create(textView);
+        }
+    }
+}
